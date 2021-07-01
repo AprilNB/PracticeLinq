9a0ef5e0bb001f58c0e0f6dfed8459651242f110
April Bullard <aprilnb86@gmail.com>
2021-06-30 20:26:24 -0500

    initial commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> gameNames = new List<string>{"Minecraft", "Raft", "Lego World", "Slime Rancher", "Mario Maker"};
+
+            IEnumerable<string> longGameNames = gameNames
+                .Where(name => name.Length >= 5)
+                .OrderBy(name => name.Length);
+        }
+    }
+}
